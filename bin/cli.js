06a66e9538119367226c86d5749127b8e96beb62
Lance Ball <lball@redhat.com>
2020-09-03 10:03:55 -0400

    src: add CLI for running via npx (#53)
    * src: add CLI for running via npx
    
    This commit adds a simple CLI binary for running functions without them
    having an explicit dependency on this module. For example, place a simple
    function in `./index.js` and you can now run it with this CLI.
    
    ```
    npx @redhat/faas-js-runtime ./index.js
    ```
    
    This will enable us to remove the `local.js` file from the faas binary.
    
    Fixes: https://github.com/boson-project/faas-js-runtime/issues/52
    See: https://github.com/boson-project/faas/pull/98

---
+++ bin/cli.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const runtime = require('../');
+const pkg = require('../package.json');
+
+const chalk = require('chalk');
+const ON_DEATH = require('death')({ uncaughtException: true });
+const { Command } = require('commander');
+
+// eslint-disable-next-line no-console
+const log = console.log;
+const program = new Command();
+
+program
+  .version(pkg.version)
+  .arguments('<file>')
+  .action(runServer);
+
+program.parse(process.argv);
+
+function runServer(file) {
+  try {
+    const func = require(extractFullPath(file));
+    runtime(func, server => {
+      ON_DEATH(_ => {
+        server.close();
+        log(chalk.yellow(`
+Goodbye!
+        `));
+      });
+
+      log(chalk.blue(`
+The server has started.
+
+You can use curl to POST an event to the endpoint:
+
+curl -X POST -d '{"hello": "world"}' \\
+    -H'Content-type: application/json' \\
+    -H'Ce-id: 1' \\
+    -H'Ce-source: cloud-event-example' \\
+    -H'Ce-type: dev.knative.example' \\
+    -H'Ce-specversion: 1.0' \\
+    http://localhost:8080
+  `));
+    });
+  } catch (error) {
+    log(chalk.redBright('Something went wrong'));
+    log(chalk.red(error));
+  }
+}
+
+function extractFullPath(file) {
+  if (path.isAbsolute(file)) return file;
+  return path.join(process.cwd(), file);
+}
