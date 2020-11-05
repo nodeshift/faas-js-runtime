9d5913d8f305cb762645ca7cd8520eb1e7968dd6
Lance Ball <lball@redhat.com>
2020-11-05 15:31:56 -0500

    fix: better error handling when network address is already in use
    Fixes: https://github.com/boson-project/faas-js-runtime/issues/73
    
    Signed-off-by: Lance Ball <lball@redhat.com>

--- bin/cli.js
+++ bin/cli.js
@@ -20,34 +20,32 @@ program
 program.parse(process.argv);
 
 function runServer(file) {
-  try {
-    const func = require(extractFullPath(file));
-    runtime(func, server => {
-      ON_DEATH(_ => {
-        server.close();
-        log(chalk.yellow(`
+  const func = require(extractFullPath(file));
+  runtime(func, server => {
+    ON_DEATH(_ => {
+      server.close();
+      log(chalk.yellow(`
 Goodbye!
-        `));
-      });
-
-      log(chalk.blue(`
-The server has started.
-
-You can use curl to POST an event to the endpoint:
-
-curl -X POST -d '{"hello": "world"}' \\
-    -H'Content-type: application/json' \\
-    -H'Ce-id: 1' \\
-    -H'Ce-source: cloud-event-example' \\
-    -H'Ce-type: dev.knative.example' \\
-    -H'Ce-specversion: 1.0' \\
-    http://localhost:8080
-  `));
+      `));
     });
-  } catch (error) {
+  }).then(_ => {
+    log(chalk.blue(`
+    The server has started.
+    
+    You can use curl to POST an event to the endpoint:
+    
+    curl -X POST -d '{"hello": "world"}' \\
+        -H'Content-type: application/json' \\
+        -H'Ce-id: 1' \\
+        -H'Ce-source: cloud-event-example' \\
+        -H'Ce-type: dev.knative.example' \\
+        -H'Ce-specversion: 1.0' \\
+        http://localhost:8080
+      `));    
+  }).catch(error => {
     log(chalk.redBright('Something went wrong'));
     log(chalk.red(error));
-  }
+  });
 }
 
 function extractFullPath(file) {
