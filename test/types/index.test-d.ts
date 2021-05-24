d3dae1dee78d9466595c828a169ff964434d28a6
Lance Ball <lball@redhat.com>
2021-05-24 14:49:25 -0400

    fix: use cloudevent as second param in test fn (#99)
    Signed-off-by: Lance Ball <lball@redhat.com>

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -2,17 +2,18 @@ import { expectType } from 'tsd';
 
 import { Server } from 'http';
 import { start, Invokable, Context, LogLevel, InvokerOptions } from '../../index';
+import { CloudEvent } from 'cloudevents';
 
 const fn: Invokable = function(
   context: Context,
-  data: any
+  cloudevent?: CloudEvent
 ) {
   return undefined;
-}
+};
 
 const options: InvokerOptions = {
   logLevel: LogLevel.info,
   port: 8080
-}
+};
 
 expectType<Promise<Server>>(start(fn, options));
