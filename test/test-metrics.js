2d74f4ce0f08a4d03739773e62dc5b3d3024a96a
Lance Ball <lball@redhat.com>
2021-11-12 14:09:40 -0500

    fixup: simplify metrics tests
    Signed-off-by: Lance Ball <lball@redhat.com>

--- test/test-metrics.js
+++ test/test-metrics.js
@@ -2,11 +2,6 @@ const { start } = require('..');
 const test = require('tape');
 const request = require('supertest');
 
-const errHandler = t => err => {
-    t.error(err);
-    t.end();
-};
-
 test('Exposes a /metrics endpoint', async t => {
   const server = await start(_ => _);
   try {
@@ -16,125 +11,20 @@ test('Exposes a /metrics endpoint', async t => {
   }
 });
 
-test('Provides an invocation count', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_invocations counter'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
+const tests = [
+  {test: 'Invocation count', expected: 'faas_invocations counter'},
+  {test: 'Error count', expected: 'faas_errors counter'},
+  {test: 'Cold start latency', expected: 'faas_cold_start_latency histogram'},
+  {test: 'Execution latency', expected: 'faas_execution_latency histogram'},
+  {test: 'Queue latency', expected: 'faas_queue_latency histogram'},
+  {test: 'CPU utilization', expected: 'faas_cpu_utilization gauge'},
+  {test: 'Memory utilization', expected: 'faas_mem_utilization gauge'},
+  {test: 'Network I/O utilization', expected: 'faas_netio_utilization gauge'},
+];
 
-test('Provides execution latency', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_queue_latency histogram'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides error count', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_errors counter'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides cold start latency', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_cold_start_latency histogram'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides queue latency', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_queue_latency histogram'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides CPU utilization', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_cpu_utilization gauge'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides memory utilization', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_mem_utilization gauge'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Provides network I/O utilization', t => {
-  t.plan(2);
-  start(_ => _)
-  .then(server => {
-    request(server)
-      .get('/metrics')
-      .end((err, got) => {
-        t.ok(got.text.includes('# TYPE faas_netio_utilization gauge'));
-        t.error(err, 'No error');
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
+for (const tc of tests) {
+  test(tc.test, async t => testMetricsInclude(`# TYPE ${tc.expected}`, t));
+}
 
 test('Only cacluates metrics for calls to /', async t => {
   const metric = 'faas_invocations{faas_name="anonymous",faas_id="",faas_instance="",faas_runtime="Node.js"}';
@@ -164,4 +54,15 @@ async function requestMetricsAndValidate(server, t, expected) {
 
 async function callEndpointNoError(server, endpoint, t) {
   return request(server).get(endpoint).catch(t.error);
+}
+
+async function testMetricsInclude(metric, t) {
+  t.plan(1);
+  const server = await start(_ => _);
+  try {
+    const got = await callEndpointNoError(server, '/metrics', t);
+    t.ok(got.text.includes(metric), metric);
+  } finally {
+    server.close();
+  }
 }
