e5636d0b8cb254de47b8c153945f3495fe666b3f
fc7 <firmicus04@gmail.com>
2023-07-25 09:46:56 +0200

    Improve check for user-provided headers

--- test/fixtures/response-structured/index.js
+++ test/fixtures/response-structured/index.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return {
     headers: {
-      'content-type': 'image/jpeg'
+      'Content-Type': 'image/jpeg'
     },
     body: Buffer.from([0xd8, 0xff, 0xe0, 0xff, 0x10, 0x00, 0x46]),
     statusCode: 201,
