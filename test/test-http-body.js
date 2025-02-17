6c8055f7c23a7d94e6424850058b71ee327d5ce0
Jitendra Kumar <contactjittu@gmail.com>
2025-02-18 02:15:03 +0530

    feat: Add configurable request body size limit option (#380)
    * feat: Add configurable request body size limit option

--- test/test-http-body.js
+++ test/test-http-body.js
@@ -135,3 +135,91 @@ test('Provides HTTP POST empty body as empty string parameter and rawBody', t =>
       });
   }, errHandler(t));
 });
+
+test('Rejects HTTP POST exceeding bodyLimit', t => {
+  const largeBody = 'x'.repeat(1048576 + 1);
+  t.plan(2);
+  start((_, receivedBody) => receivedBody, { bodyLimit: 1048576 })
+    .then(server => {
+      request(server)
+        .post('/')
+        .send(largeBody)
+        .set({ 'Content-Type': 'text/plain' })
+        .expect(413)
+        .end((err, res) => {
+          t.error(err, 'No error');
+          t.equal(res.status, 413, 'Should reject payload larger than limit');
+          t.end();
+          server.close();
+        });
+    }, errHandler(t));
+});
+
+test('Accepts HTTP POST within bodyLimit', t => {
+  const body = 'x'.repeat(1048576);
+  t.plan(2);
+  start((_, receivedBody) => {
+    t.equal(receivedBody, body);
+    return receivedBody;
+  }, { bodyLimit: 1048576 })
+    .then(server => {
+      request(server)
+        .post('/')
+        .send(body)
+        .set({ 'Content-Type': 'text/plain' })
+        .expect('Content-Type', /plain/)
+        .expect(200)
+        .end((err, _) => {
+          t.error(err, 'No error');
+          t.end();
+          server.close();
+        });
+    }, errHandler(t));
+});
+
+test('Rejects payload exceeding environment variable bodyLimit', t => {
+  const bodyLimit = 1048576;
+  const largeBody = 'x'.repeat(bodyLimit + 1);
+  process.env.FUNC_BODY_LIMIT = bodyLimit;
+  t.plan(2);
+  start((_, receivedBody) => receivedBody)
+    .then(server => {
+      request(server)
+        .post('/')
+        .send(largeBody)
+        .set({ 'Content-Type': 'text/plain' })
+        .expect(413)
+        .end((err, res) => {
+          t.error(err, 'No error');
+          t.equal(res.status, 413, 'Should reject payload larger than env var limit');
+          delete process.env.FUNC_BODY_LIMIT;
+          t.end();
+          server.close();
+        });
+    }, errHandler(t));
+});
+
+test('Accepts payload within environment variable bodyLimit', t => {
+  const bodyLimit = 524288;
+  const body = 'x'.repeat(bodyLimit);
+  process.env.FUNC_BODY_LIMIT = bodyLimit;
+  t.plan(2);
+  start((_, receivedBody) => {
+    t.equal(receivedBody, body);
+    return receivedBody;
+  })
+    .then(server => {
+      request(server)
+        .post('/')
+        .send(body)
+        .set({ 'Content-Type': 'text/plain' })
+        .expect('Content-Type', /plain/)
+        .expect(200)
+        .end((err, _) => {
+          t.error(err, 'No error');
+          delete process.env.FUNC_BODY_LIMIT;
+          t.end();
+          server.close();
+        });
+    }, errHandler(t));
+});
